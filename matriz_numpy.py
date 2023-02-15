a68cb83d6a1c0b4f56a7acc307c4d5b1854318af
serchgiles <sergio.ivan_1224@hotmail.com>
2023-02-15 17:45:20 -0600

    Actividad 4 Arrays Bidimensionales (modificacion)

--- matriz_numpy.py
+++ matriz_numpy.py
@@ -4,6 +4,6 @@ print("Creamos una matriz de 3x3 con valores del 0 al 8")
 matriz = np.reshape(np.array(range(0,9)),(3,3))
 print(matriz)
 
-print("\nAhora una matriz identidad de 6x6")
+print("\nAhora una matriz identidad de 6x6 (tambien se puede crear con la funcion identity")
 iden6 = np.eye(6)
 print(iden6)
