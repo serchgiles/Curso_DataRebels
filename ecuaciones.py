444a597e014eca8e848cc0558176896b57309873
serchgiles <sergio.ivan_1224@hotmail.com>
2023-02-15 18:17:16 -0600

    Actividad 5 Operaciones Edad

---
+++ ecuaciones.py
@@ -0,0 +1,18 @@
+print("El doble de mi edad tiene 24 años")
+print("Ecuación: 2x = 24")
+x = 24/2
+
+print("\nx = "+str(x))
+
+print("A un tercio de la edad de mi hermana le disminuyo 15 años para tener la misma edad que yo")
+print("y/3 -15 = 6")
+y= (6+15)*3
+
+print("\ny = "+str(y))
+
+if x < y:
+	print("x es más chico que y")
+elif x > y:
+	print("y es más chico que x")
+else:
+	print("Ambos tienen la misma edad")
